c751e6fd376c00f21d8bd2e03642915510e96ee5
Ghazouani Sami <ghazouanisami100stic@gmail.com>
2020-05-26 09:50:15 +0100

    first commit

---
+++ emotion/constants.py
@@ -0,0 +1,4 @@
+class Constant:
+	MODEL_PATH = '/home/ghazouani/Bureau/final/saved_models/Emotion_Voice_Detection_Model.h5'
+	LABELS_PATH = '/home/ghazouani/Bureau/final/label_classes.npy'
+	INPUT_AUDIO_PATH = '/home/ghazouani/Bureau/emotion voice detection/emotion/live_audio.wav'
